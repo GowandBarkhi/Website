046ea48d0e864d7fd88d11019bd685feebd01dec
Gowand Barkhi <gowandbarkhi@gmail.com>
2026-02-15 23:09:17 +0100

    Termine hinzugefügt

---
+++ termine-data.js
@@ -0,0 +1,65 @@
+// ========================================
+// TERMINE-DATEN
+// Diese Datei wird von termine.html und termine-insta.html geladen
+// ========================================
+
+var termine = [
+  {
+    datum: "25. Februar 2026",
+    titel: "Nightwash",
+    ort: "Köln",
+    ticketLink: "https://www.eventim.de/event/nightwash-live-tour-2026-gloria-20554849/?msockid=22eac5d628a763da3146d11c292c6207",
+    sichtbar: true
+  },
+{
+    datum: "19. Februar 2026",
+    titel: "Der Dompteur lädt ein - Comedy Mixed Show",
+    ort: "Frankfurt am Main",
+    ticketLink: "https://www.eventim.de/event/der-dompteur-laedt-ein-comedy-mixed-show-3524028.20744453/",
+    sichtbar: true
+  },
+{
+  datum: "05. März 2026",
+  titel: "Blueprint Comedy - Lachen & Wein",
+  ort: "Mülheim an der Ruhr",
+  ticketLink: "https://www.eventim.de/event/blueprint-comedy-lachen-wein-muelheim-an-der-ruhr-clique-wine-coffee-21180064/?srsltid=AfmBOor_FOay_BIQwWuARkP4gmW1niRvE4cazx3KMlOAUceofPfLGOO2",
+  sichtbar: true
+},
+{
+  datum: "20. Februar 2026",
+  titel: "Roh Comedy StandUp Show",
+  ort: "Düsseldorf",
+  ticketLink: "https://www.eventbrite.de/e/roh-comedy-standup-in-dusseldorf-tickets-1841681501949",
+  sichtbar: true
+},
+{
+  datum: "26. Februar 2026",
+  titel: "Bonner Comedy Slam #5",
+  ort: "Bonn",
+  ticketLink: "https://eventfrog.de/de/p/theater-buehne/comedy-kabarett/bonner-comedy-slam-5-2025-26-7418574884928765877.html",
+  sichtbar: true
+},
+{
+  datum: "07. März 2026",
+  titel: "Heddesheimer Comedy Preis 2026 - \"Heddesheimer Tor\"",
+  ort: "Heddesheim",
+  ticketLink: "https://eventfrog.de/de/p/theater-buehne/comedy-kabarett/heddesheimer-comedy-preis-2026-heddesheimer-tor-7382083760165015173.html",
+  sichtbar: true
+},
+{
+  datum: "21. Februar 2026",
+  titel: "Comedy Show Vechta",
+  ort: "Vechta",
+  ticketLink: "https://www.eventim.de/eventsd/comedy-shows/vechta-336/?srsltid=AfmBOop3J0ktZ-UIsP2yvKjjzVhhfdQdeKGBEYHfG4Wadkp-YpWLw6rm",
+  visible: true
+},
+{
+  datum: "20. Februar 2026",
+  titel: "BlackWay Comedy",
+  ort: "Jüchen",
+  ticketLink: "https://www.eventbrite.de/e/blackway-comedy-part-ii-tickets-1962761329974",
+  sichtbar: true
+}
+
+
+];
