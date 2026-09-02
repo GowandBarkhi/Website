eb18e050f00e5cfc08dfc9472c5575ad4de1b518
GowandBarkhi <gowand.barkhi@uni-duesseldorf.de>
2026-09-02 15:03:49 +0200

    termine

--- termine-data.js
+++ termine-data.js
@@ -5,45 +5,45 @@
 
 var termine = [
   {
-    datum: "25. Februar 2026",
-    titel: "Nightwash",
-    ort: "Köln",
-    ticketLink: "https://www.eventim.de/event/nightwash-live-tour-2026-gloria-20554849/?msockid=22eac5d628a763da3146d11c292c6207",
+    datum: "04. September 2026",
+    titel: "Fun Fun Comedy Night",
+    ort: "Hilden",
     sichtbar: true
   },
 {
-    datum: "19. Februar 2026",
-    titel: "Der Dompteur lädt ein - Comedy Mixed Show",
-    ort: "Frankfurt am Main",
-    ticketLink: "https://www.eventim.de/event/der-dompteur-laedt-ein-comedy-mixed-show-3524028.20744453/",
+    datum: "05. September 2026",
+    titel: "Comedy Flash",
+    ort: "Remscheid",
     sichtbar: true
   },
 {
-  datum: "05. März 2026",
-  titel: "Blueprint Comedy - Lachen & Wein",
-  ort: "Mülheim an der Ruhr",
-  ticketLink: "https://www.eventim.de/event/blueprint-comedy-lachen-wein-muelheim-an-der-ruhr-clique-wine-coffee-21180064/?srsltid=AfmBOor_FOay_BIQwWuARkP4gmW1niRvE4cazx3KMlOAUceofPfLGOO2",
-  sichtbar: true
-},
-{
-  datum: "20. Februar 2026",
-  titel: "Roh Comedy StandUp Show",
-  ort: "Düsseldorf",
-  ticketLink: "https://www.eventbrite.de/e/roh-comedy-standup-in-dusseldorf-tickets-1841681501949",
-  sichtbar: true
-},
-{
-  datum: "26. Februar 2026",
-  titel: "Bonner Comedy Slam",
+  datum: "06. September 2026",
+  titel: "Cozy Comedy",
   ort: "Bonn",
-  ticketLink: "https://eventfrog.de/de/p/theater-buehne/comedy-kabarett/bonner-comedy-slam-5-2025-26-7418574884928765877.html",
   sichtbar: true
 },
 {
-  datum: "07. März 2026",
-  titel: "Heddesheimer Comedy Preis 2026 - \"Heddesheimer Tor\"",
-  ort: "Heddesheim",
-  ticketLink: "https://eventfrog.de/de/p/theater-buehne/comedy-kabarett/heddesheimer-comedy-preis-2026-heddesheimer-tor-7382083760165015173.html",
+  datum: "22. September 2026",
+  titel: "Stuttgarter Comedy Clash",
+  ort: "Stuttgart",
+  sichtbar: true
+},
+{
+  datum: "25. September 2026",
+  titel: "Haha Comedy",
+  ort: "Köln",
+  sichtbar: true
+},
+{
+  datum: "26. September 2026",
+  titel: "Haha Comedy",
+  ort: "Köln",
+  sichtbar: true
+},
+{
+  datum: "03. Oktober 2026",
+  titel: "Nightwash -Comedy Club",
+  ort: "Köln",
   sichtbar: true
 },
 {
@@ -54,10 +54,9 @@ var termine = [
   visible: true
 },
 {
-  datum: "20. März 2026",
+  datum: "20. März 2028",
   titel: "BlackWay Comedy Part III",
   ort: "Jüchen",
-  ticketLink: "https://www.eventbrite.de/e/blackway-comedy-part-iii-tickets-1962764317911",
   sichtbar: true
 },
 {
